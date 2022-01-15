b056e6ef48d2baba518d8a0eb7b96c2a8ed4168a
hrishikesh <hshinde166@gmail.com>
2022-01-16 01:44:09 +0530

    Adding the Row components and passing certain props to them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Row from './Components/Row';
+import { requests } from './tmdb api/requests';
 
 function App() {
   return (
     <div className="App">
-      hey there
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending" />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+      <Row Top-Rated="Trending" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action-Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy-Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror-Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance-Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
